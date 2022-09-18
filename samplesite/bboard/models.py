47ff1b0b57674b4b36c128db4456c95e9b89a22b
sinarhen <nicklchannel8@gmail.com>
2022-09-18 11:24:15 +0300

    First model

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Bb(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    content = models.TextField(verbose_name='Content', null=True)
+    price = models.FloatField(verbose_name='Price', null=True)
+    published = models.DateTimeField(auto_now_add=True, verbose_name='Published')
+
