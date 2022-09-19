b6cbf44844df689a559bbee5059ed8454e34bc1e
sinarhen <nicklchannel8@gmail.com>
2022-09-19 17:42:15 +0300

    First model

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -3,8 +3,26 @@ from django.db import models
 
 # Create your models here.
 class Bb(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Title')
-    content = models.TextField(verbose_name='Content', null=True)
-    price = models.FloatField(verbose_name='Price', null=True)
-    published = models.DateTimeField(auto_now_add=True, verbose_name='Published')
+    title = models.CharField(max_length=50, verbose_name='Title')
+    content = models.TextField(verbose_name='Content', null=True, blank=True)
+    price = models.FloatField(verbose_name='Price', null=True, blank=True)
+    published = models.DateTimeField(auto_now_add=True, verbose_name='Published', db_index=True)
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubric')
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Ad'
+        verbose_name_plural = 'Ads'
+        ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Name')
+
+    class Meta:
+        verbose_name = 'Rubric'
+        verbose_name_plural = 'Rubrics'
+        ordering = ['name']
+        
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,10 @@
+from .models import Bb
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('asdadsdas')
-
+    bbs = Bb.objects.order_by('-published')
+    context = {
+        'bbs': bbs
+    }
+    return render(request, template_name="index.html", context=context)
