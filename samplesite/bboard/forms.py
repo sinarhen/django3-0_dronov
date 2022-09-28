bd6fb2221197bc7c522e4eec2e9e5bb9dc52660b
sinarhen <nicklchannel8@gmail.com>
2022-09-28 15:37:27 +0300

    Test

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,8 +1,38 @@
-from django.forms import ModelForm
+from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Bb
 
 
-class BbForm(ModelForm):
+class BbForm(forms.ModelForm):
     class Meta:
         model = Bb
-        fields = ('title', 'content', 'price', 'rubric',)
+        fields = ('title', 'content', 'price', 'rubric', 'kind')
+
+    # def clean_kind(self):
+    #     """Field validation level"""
+    #     data = self.cleaned_data['kind']
+    #     if 'yourdomain@gmail.com' not in data:
+    #         raise ValidationError('Invalid')
+    #     return data
+    #
+    # def clean_price(self):
+    #     """Field validation level"""
+    #     data = self.cleaned_data.get('price')
+    #     if data > 1:
+    #         raise ValidationError('Should be lower than 1')
+    #
+    # def clean(self):
+        """Form validation level"""
+    #     cleaned_data = super(BbForm, self).clean()
+    #
+    #     kind_passed = cleaned_data.get('kind')
+    #     kind_req = 'Fred'
+    #
+    #     price_passed = cleaned_data.get('price')
+    #     price_req = 1000
+    #     if price_req == 1000:
+    #         raise ValidationError('Not equal 1000')
+    #     if not kind_req in kind_passed:
+    #         raise ValidationError('Not a valid kind')
+    #     return cleaned_data
--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,13 +1,46 @@
 from django.db import models
+from .validators import *
 
+
+class Machine(models.Model):
+    name = models.CharField(max_length=55, null=False, blank=False)
+    detail = models.ManyToManyField('Detail')
+
+
+class Detail(models.Model):
+    name = models.CharField(max_length=55, null=False, blank=False)
 
 # Create your models here.
+
+
 class Bb(models.Model):
+    KINDS = ({
+                 None: 'What are we doing?',
+                 'a': 'Buying',
+                 'b': 'Selling',
+                 'c': 'Switching',
+             }.items())
     title = models.CharField(max_length=50, verbose_name='Title')
     content = models.TextField(verbose_name='Content', null=True, blank=True)
     price = models.FloatField(verbose_name='Price', null=True, blank=True)
     published = models.DateTimeField(auto_now_add=True, verbose_name='Published', db_index=True)
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubric')
+    kind = models.CharField(verbose_name='Kind', choices=KINDS, default='s', max_length=4)
+
+    def clean(self):
+        errors = {}
+        if self.content and len(self.content) < 40:
+            errors['content'] = ValidationError('At least 40 symbols')
+        elif self.price and self.price <= 100:
+            errors['price'] = ValidationError('Price should be at least 100')
+        if errors:
+            """
+            errors = {
+                    'content': ValidationError('At least 40 symbols),
+                    'price': ValidationError('Price should be at least 100'),
+                }
+            """
+            raise ValidationError(errors)
 
     def __str__(self):
         return self.title
