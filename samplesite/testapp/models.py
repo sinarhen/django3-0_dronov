83528fc7d1dbcd454294952e266325fae20cf16a
sinarhen <nicklchannel8@gmail.com>
2022-11-01 12:35:19 +0200

    Many to many: through

--- samplesite/testapp/models.py
+++ samplesite/testapp/models.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    content = models.TextField()
+
+
+class PrivateMessage(Message):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.OneToOneField(Message, on_delete=models.CASCADE, parent_link=True)
+
+
 class Spare(models.Model):
     name = models.CharField(max_length=55, null=False, blank=False)
 
@@ -16,3 +28,13 @@ class Kit(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     spare = models.ForeignKey(Spare, on_delete=models.CASCADE)
     count = models.IntegerField()
+
+
+class Note(models.Model):
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey(ct_field='content_type', fk_field='object_id')
+
+
+print(ContentType)
