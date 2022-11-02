4eec76d41392ce067517209cfa8f92a57e520c68
sinarhen <nicklchannel8@gmail.com>
2022-11-02 14:26:59 +0200

    Transactions, atomic

--- samplesite/testapp/views.py
+++ samplesite/testapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.db import transaction
+
 
 # Create your views here.
+
+
+@transaction.non_atomic_requests
+def my_view(request):
+    return None
+
+
+@transaction.atomic
+def edit(request, pk):
+    return None
