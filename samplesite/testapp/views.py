aa196f4b3673419bed2b9b3623374370a9e3c11d
sinarhen <nicklchannel8@gmail.com>
2022-11-03 11:00:08 +0200

    Transactions

--- samplesite/testapp/views.py
+++ samplesite/testapp/views.py
@@ -1,5 +1,7 @@
+from django.forms import formset_factory
 from django.shortcuts import render
 from django.db import transaction
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -13,3 +15,34 @@ def my_view(request):
 @transaction.atomic
 def edit(request, pk):
     return None
+
+
+def manual_transaction_control(request):
+    form = UserCreationForm()
+    if form.is_valid():
+        try:
+            form.save()
+            transaction.commit()
+        except:
+            transaction.rollback()
+
+
+def manual_transaction_control_formset(request):
+    formset = formset_factory(UserCreationForm, extra=2)()
+    if formset.is_valid():
+        for form in formset:
+            if form.cleaned_data:
+                sp = transaction.savepoint()
+                try:
+                    form.save()
+                    transaction.savepoint_commit()
+                except:
+                    transaction.savepoint_rollback()
+                transaction.commit()
+
+
+def commit_handler():
+    """some actions when transaction verified"""
+
+
+transaction.on_commit(commit_handler)
