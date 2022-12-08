affc47a71b1f05ab1cbd658c7910245e78000769
sinarhen <nicklchannel8@gmail.com>
2022-12-08 14:26:12 +0200

    Admin panel

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -4,3 +4,16 @@ from .models import Bb, Rubric
 # Register your models here.
 admin.site.register(Bb)
 admin.site.register(Rubric)
+
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title_and_rubric',
+                    'content',
+                    'price',
+                    'published',
+                    )
+
+    def title_and_price(self):
+        ...
+
+    title_and_price.short
